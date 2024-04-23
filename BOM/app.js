b014f7c93f28613a306f06fa62b0069a2f007c2d
Pablo Marino <mentor.rolling@gmail.com>
2024-04-23 16:31:39 -0300

    BOM e introducción al DOM

--- BOM/app.js
+++ BOM/app.js
@@ -25,3 +25,31 @@ const dondeEstoy = () => {
     console.error("Geolocalización no es soportada en el navegador");
   }
 };
+
+//Funciones de tiempo
+
+let intervalo = null;
+
+const relojBom = () => {
+  intervalo = setInterval(() => {
+    let hora = new Date().getHours();
+    let minutos = new Date().getMinutes();
+    let segundos = new Date().getSeconds();
+
+    let tiempo = `${hora}:${minutos}:${segundos}`;
+
+    document.body.innerText = "";
+    document.write(`<h3>${tiempo}</h3>`);
+  }, 1000);
+};
+
+const detenerReloj = () => {
+  clearInterval(intervalo);
+};
+
+const redirigir = () => {
+  document.write("<h3>Redireccionando a la página principal...</h3>");
+  setTimeout(() => {
+    location.assign("https://google.com");
+  }, 3000);
+};
---
+++ DOM/app.js
@@ -0,0 +1,7 @@
+// ---Traer datos capturando el elemento----
+document.getElementById("id"); //traigo la etiqueta con el id
+document.getElementsByClassName("classname"); //traigo la clase
+document.getElementsByTagName("tag"); //traigo el elemento por su y tag ('p')
+document.querySelector("#id");
+document.querySelector(".clase");
+document.querySelectorAll("p");
